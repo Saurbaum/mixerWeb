65cb14c77df8a40f04d09e24c469a6816184e5e0
Ian Newborn <ian_newborn@outlook.com>
2015-09-25 18:00:42 +0100

    Change root
    Swapped test path for setVolume

--- mixer.go
+++ mixer.go
@@ -7,17 +7,17 @@ import (
 	"net/http"
 )
 
-func test(w http.ResponseWriter, r *http.Request) {
+func setVolumes(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	body, _ := ioutil.ReadAll(r.Body)
 	var bodyText = string(body)
 	fmt.Println(bodyText)
-	io.WriteString(w, "Testing testing 1 2 3. "+bodyText)
+	io.WriteString(w, bodyText)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("www")))
-	mux.HandleFunc("/test", test)
+	mux.HandleFunc("/setVolumes", setVolumes)
 	http.ListenAndServe(":8000", mux)
 }
