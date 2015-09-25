1279d0a2bb66bc406659312339e0efb3536e73d8
Ian Newborn <saurbaum@gmail.com>
2015-09-25 22:16:16 +0100

    Playback controls
    Added playback control options in web server.
    Finished volume button theme.

--- mixer.go
+++ mixer.go
@@ -15,9 +15,34 @@ func setVolumes(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, bodyText)
 }
 
+func play(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("play")
+}
+
+func pause(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("pause")
+}
+
+func stop(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("stop")
+}
+
+func previous(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("previous")
+}
+
+func next(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("next")
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("www")))
 	mux.HandleFunc("/setVolumes", setVolumes)
+	mux.HandleFunc("/play", play)
+	mux.HandleFunc("/pause", pause)
+	mux.HandleFunc("/stop", stop)
+	mux.HandleFunc("/previous", previous)
+	mux.HandleFunc("/next", next)
 	http.ListenAndServe(":8000", mux)
 }
