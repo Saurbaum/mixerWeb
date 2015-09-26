a6a3aeb43d98a84865e583ba2497793cad548e64
Ian Newborn <saurbaum@gmail.com>
2015-09-26 14:14:08 +0100

    Track playback
    Added playback volume slider
    Added track selection

--- mixer.go
+++ mixer.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 )
+
+type Track struct {
+	Name string
+	Id   int
+}
 
 func setVolumes(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -17,6 +23,11 @@ func setVolumes(w http.ResponseWriter, r *http.Request) {
 
 func play(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("play")
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	var bodyText = string(body)
+	fmt.Println(bodyText)
+	io.WriteString(w, bodyText)
 }
 
 func pause(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +46,28 @@ func next(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("next")
 }
 
+func playbackVolume(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	var bodyText = string(body)
+	fmt.Println(bodyText)
+}
+
+func trackList(w http.ResponseWriter, r *http.Request) {
+
+	var tracks []Track
+	tracks = append(tracks, Track{"Track 1", 0})
+	tracks = append(tracks, Track{"Track 2", 1})
+	tracks = append(tracks, Track{"Track 3", 2})
+	tracks = append(tracks, Track{"Track 4", 3})
+	tracks = append(tracks, Track{"Track 5", 4})
+
+	b, _ := json.Marshal(tracks)
+
+	fmt.Println(string(b))
+	io.WriteString(w, string(b))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("www")))
@@ -44,5 +77,7 @@ func main() {
 	mux.HandleFunc("/stop", stop)
 	mux.HandleFunc("/previous", previous)
 	mux.HandleFunc("/next", next)
+	mux.HandleFunc("/playbackVolume", playbackVolume)
+	mux.HandleFunc("/trackList", trackList)
 	http.ListenAndServe(":8000", mux)
 }
